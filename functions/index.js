5af7bae1eed36005f5d4d36eb67715a8438f288c
byio <ben@benyang.io>
2018-03-31 18:47:57 +0800

    create an empty create_user function, export function via index.js to be used by google cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
 const functions = require('firebase-functions');
 
+// import functions
+const createUser = require('./create_user');
+
+// export function to be used by google cloud functions
+exports.createUser = functions.https.onRequest(createUser);
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
