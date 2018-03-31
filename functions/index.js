1f76e46c015f135c0972f90f8a7b6617a04a5e4e
byio <ben@benyang.io>
2018-03-31 18:48:39 +0800

    clean up index.js

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,3 @@ const createUser = require('./create_user');
 
 // export function to be used by google cloud functions
 exports.createUser = functions.https.onRequest(createUser);
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
