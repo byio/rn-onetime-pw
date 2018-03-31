19c75fc8b8bfccf68c141cb6cbaeb9e08d6ad96e
byio <ben@benyang.io>
2018-03-31 19:00:48 +0800

    retrieve service account credentials, and create .gitignore file to ignore the json file with the service account details when pushing to github

--- functions/index.js
+++ functions/index.js
@@ -3,5 +3,11 @@ const functions = require('firebase-functions');
 // import functions
 const createUser = require('./create_user');
 
+// initialize service account
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://rn-onetime-pw.firebaseio.com"
+});
+
 // export function to be used by google cloud functions
 exports.createUser = functions.https.onRequest(createUser);
