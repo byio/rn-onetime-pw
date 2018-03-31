f1e617ae1143cd6781f81753fc0aec880436fc0a
byio <ben@benyang.io>
2018-03-31 21:07:00 +0800

    in create_user function, check that a phone number input is provided, and format the input to include only stringified digits

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,12 @@
 module.exports = (req, res) => {
+  // verify that phone number (req.body.phone) is provided
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+  // format phone number (convert to string, and remove anything that's not a number)
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  // create a new user account using phone number
   
+  // respond to user request, saying that account was created
 };
