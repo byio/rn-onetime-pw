9bfa3e073a21ad081e934920e39cde4ee58a723f
byio <ben@benyang.io>
2018-04-02 20:25:03 +0800

    find user with phone number, and attempt to match otp. update codeValid property to false upon match

--- functions/verify_otp.js
+++ functions/verify_otp.js
@@ -1,3 +1,5 @@
+const admin = require('firebase-admin');
+
 // create and export verifyOtp function
 modules.export = (req, res) => {
   // check for phone number and otp
@@ -7,5 +9,25 @@ modules.export = (req, res) => {
   // format phone number and otp
   const phone = String(req.body.phone).replace(/[^\d]/g, "");
   const code = parseInt(req.body.code);
-  
+  // find user using phone number, and attempt to match otp
+  admin.auth()
+       .getUser(phone)
+       .then(userRecord => {
+         // create a reference to the user
+         const ref = admin.database().ref('users/' + phone);
+         // listen to value changes in ref
+         ref.on('value', snapshot => {
+           const userRecord = snapshot.val();
+           // if otp doesn't match or has expired
+           if (userRecord.code !== code || !user.codeValid) {
+             return res.status(422).send({ error: 'Invalid code.' });
+           }
+           // update codeValid property
+           ref.update({ codeValid: false });
+         });
+       })
+       .catch(err => {
+         // if user cannot be found
+         res.status(422).send({ error: err });
+       });
 };
