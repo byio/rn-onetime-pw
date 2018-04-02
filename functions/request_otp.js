b8bdcc02a3df18af845faec31717c6d64dc872d0
byio <ben@benyang.io>
2018-04-02 19:02:42 +0800

    text otp to user, and save otp to user model in firebase db

--- functions/request_otp.js
+++ functions/request_otp.js
@@ -1,5 +1,8 @@
 const admin = require('firebase-admin');
 
+const twilioCred = require('./twilio_cred');
+const twilio = require('./twilio');
+
 // create and export requestOtp function
 module.exports = (req, res) => {
   // verify that phone number is provided
@@ -14,6 +17,27 @@ module.exports = (req, res) => {
        .then(userRecord => {
          // generate code to be sent
          const code = Math.floor(Math.random() * 8999 + 1000);
+         // text user
+         twilio.messages.create({
+           body: `Your code is ${code}.`,
+           to: `+${phone}`,
+           from: twilioCred.fromPhone
+         }, (err) => {
+           // if there's an error
+           if (err) {
+             return res.status(422).send({ error: err });
+           }
+           // save code to  user in firebase db if no error
+           admin.database()
+                .ref('users/' + phone)
+                .update({
+                  code: code,
+                  codeValid: true
+                }, () => {
+                  res.send({ success: true });
+                });
+         });
+         return null;
        })
        .catch(err => {
          // res.status(422).send({ error: 'User not found' })
